b466ad63f5b26f1fb731690c8806e99452b6c20c
microsarah <sarah.hallacher@gmail.com>
2017-12-17 16:25:58 -0500

    Revert "select variant to change image"
    This reverts commit fa61af3f6533ad972ac9571a78f47a75b2122fd3.

--- lets-get-sweaty/src/scripts/theme.js
+++ lets-get-sweaty/src/scripts/theme.js
@@ -52,24 +52,3 @@ $(document).ready(function() {
     document.documentElement.className = document.documentElement.className.replace('supports-no-cookies', 'supports-cookies');
   }
 });
-
-// Select product variants by clicking their images
-$(document).ready(function() {
-  thumbnails = $('img[src*="/products/"]').not(':first');
-  if (thumbnails.length) {
-    thumbnails.bind('click', function() {
-      var arrImage = $(this).attr('src').split('?')[0].split('.');
-      var strExtention = arrImage.pop();
-      var strRemaining = arrImage.pop().replace(/_[a-zA-Z0-9@]+$/,'');
-      var strNewImage = arrImage.join('.')+"."+strRemaining+"."+strExtention;
-      if (typeof variantImages[strNewImage] !== 'undefined') {
-          productOptions.forEach(function (value, i) {
-           optionValue = variantImages[strNewImage]['option-'+i];
-           if (optionValue !== null && $('.single-option-selector:eq('+i+') option').filter(function() { return $(this).text() === optionValue }).length) {
-             $('.single-option-selector:eq('+i+')').val(optionValue).trigger('change');
-           }
-        });
-      }
-    });
-  }
-});

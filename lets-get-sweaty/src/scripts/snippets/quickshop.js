407c4970cd8ae5b822f80a7761b5391f19edea71
aaron v cohen <avcohen@gmail.com>
2018-03-03 19:35:50 -0500

    connected quick add product to variants proto

--- lets-get-sweaty/src/scripts/snippets/quickshop.js
+++ lets-get-sweaty/src/scripts/snippets/quickshop.js
@@ -10,10 +10,14 @@
 
     // Define data-attribute selectors for the quickshop DOM element(s);
     var selectors = {
-        singleOptionSelectorQuickshop: '[data-single-option-selector-quickshop]',
+        comparePrice: '[data-compare-price]',
+        comparePriceText: '[data-compare-text]',
+        originalSelectorId: '[data-product-select]',
         productFeaturedImageQuickshop: '[data-collection-product-image]',
+        productJson: '[data-product-json]',
         productPrice: '[data-product-price]',
         quickAddToCart: '[data-quickadd-to-cart]',
+        singleOptionSelectorQuickshop: '[data-single-option-selector-quickshop]',
     };
 
     /**
@@ -23,24 +27,56 @@
     function Quickshop(container){
         this.$container = $(container);
         this.id = this.$container.attr('data-quickshop-product-id');
+        this.productSingleObject = JSON.parse($(selectors.productJson, this.$container).html());
 
         var options = {
             $container : this.$container,
-            enableHistoryState: this.$container.data('enable-history-state') || false,
+            product: this.productSingleObject,
             singleOptionSelector: selectors.singleOptionSelectorQuickshop,
             originalSelectorId: selectors.originalSelectorId,
-            product: this.productSingleObject,
-            productThumbs: selectors.productThumbs,
-            productFeaturedImage: selectors.productFeaturedImage,
-            productVariantImage: selectors.productVariantImage,
+            enableHistoryState: this.$container.data('enable-history-state') || false,
+            singleOptionSelector: selectors.singleOptionSelectorQuickshop,
+            productFeaturedImage: selectors.productFeaturedImage, // get from parent obj
+            // productVariantImage: selectors.productVariantImage,
         }
 
         this.namespace = '.quickshop';
-        // this.variants = new slate.Variants(options);
+        this.variants = new slate.Variants(options);
+
+        this.$container.on('variantChange' + this.namespace, this.updateAddToCartState.bind(this));
 
     };
 
     Quickshop.prototype = $.extend({}, Quickshop.prototype, {
+
+        /**
+        * Updates the DOM state of the quick add to cart button
+        *
+        * @param {boolean} enabled - Decides whether cart is enabled or disabled
+        * @param {string} text - Updates the text notification content of the cart
+        */
+        updateAddToCartState: function(evt) {
+          var variant = evt.variant;
+
+          if (variant.available) {
+            $(selectors.quickAddToCart, this.$container).prop('disabled', false);
+            $(selectors.addToCartText, this.$container).html(theme.strings.addToCart);
+          } else {
+            $(selectors.quickAddToCart, this.$container).prop('disabled', true);
+            $(selectors.addToCartText, this.$container).html(theme.strings.soldOut);
+          }
+        },
+
+        /**
+        *    Updates DOM with selected variant product price
+        *     @param {string} productPrice - Current Price
+        *     @param {string} comparePraice - Original Price
+        */
+        updateProductPrices: function(evt){
+            var variant = evt.variant;
+            var $comparePrice = $(selectors.comparePrice, this.$container);
+            var $compareEls = $comparePrice.add(selectors.comparePriceText, this.$container);
+        },
 
     });
 
